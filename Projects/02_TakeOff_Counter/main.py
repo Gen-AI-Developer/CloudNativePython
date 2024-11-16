5a9124a2d6d4a6df8347457dedf6116b0a628ed5
SafdarAliShah <programmersafdar@live.com>
2024-11-16 11:18:52 +0500

    Git Hupdated

--- Projects/02_TakeOff_Counter/main.py
+++ Projects/02_TakeOff_Counter/main.py
@@ -1,1 +1,9 @@
-seconds = int()
+for i in range(10,1,-1):
+    print(i,end=" ")
+print("Lift Off!")
+
+number_list : list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def countdown(number_list):
+    for number in number_list:
+        print(number, end=" ")
+    print("Lift Off!")
