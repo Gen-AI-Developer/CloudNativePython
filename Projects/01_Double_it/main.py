795adfa0ee9034ca3141fe4a7dfd4859f08008ce
SafdarAliShah <programmersafdar@live.com>
2024-11-16 08:50:12 +0500

    Second Project Structure Updated

--- Projects/01_Double_it/main.py
+++ Projects/01_Double_it/main.py
@@ -1,7 +1,9 @@
-number = int(input("Enter a number: "))
-while number < 10000:
-    numb = number * 222;
-    print(str(number) +" Double is :" + str(numb))
-    if numb==10000:
-        break
-    number = numb 
+def _double_it():
+        number = int(input("Enter a number: "))
+        while number < 10000:
+            numb = number * 2;
+            print(str(number) +" Double is :" + str(numb))
+            if numb==10000:
+                break
+            number = numb 
+_double_it()
